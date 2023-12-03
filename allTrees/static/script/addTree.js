a5940465fe18558c630650e133803547c01bdf68
tjaofficial <ajackerm@mtu.edu>
2023-12-03 13:21:01 -0500

    Fixed sme CSS

--- allTrees/static/script/addTree.js
+++ allTrees/static/script/addTree.js
@@ -5,7 +5,7 @@ function changeTree(){
     console.log(document.getElementById('locationID').dataset.locationid);
     for(let i=0; i<treeList.length; i++) {
         let tree = treeList[i];
-        document.getElementById(tree).innerHTML = `<a href='../../treeData/${locationID}/${areaID}/${tree}'><div id="treeDiv" class="grid-item">${tree} - tree</div></a>`
+        document.getElementById(tree).innerHTML = `<a href='../../treeData/${locationID}/${areaID}/${tree}'><div id="treeDiv" class="">${tree} - tree</div></a>`
     }
 }
 changeTree();
