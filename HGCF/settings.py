8d0ca5d633408cf445ad96140db2216bbf213b98
Anthony <ajackerm@mtu.edu>
2023-12-14 23:19:34 -0500

    fixing admin static

--- HGCF/settings.py
+++ HGCF/settings.py
@@ -130,14 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_root")
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'allTrees/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
