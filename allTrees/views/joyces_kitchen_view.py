45d9b766dca19304abfb37c6c811a9850c8278f9
Anthony <ajackerm@mtu.edu>
2023-12-16 23:40:55 -0500

    Updted Logo, Updated Recipe Form, Home Page

--- allTrees/views/joyces_kitchen_view.py
+++ allTrees/views/joyces_kitchen_view.py
@@ -14,16 +14,25 @@ def recipeForm_view(request):
     modelData = recipeModel.objects.all()
     if request.method == "POST":
         dataCopy = request.POST.copy()
+        print(dataCopy)
         iList = {}
         for x in range(1,16):
             if 'iName'+ str(x) in dataCopy:
                 iList[str(x)] = {
                     'name': dataCopy['iName'+ str(x)],
                     'qty': dataCopy['iQty'+ str(x)],
-                    'notes': dataCopy['inotes'+ str(x)]
+                    'notes': dataCopy['iNotes'+ str(x)]
                 }
         dataCopy['ingredients'] = iList
-        dataCopy['equipment'] = iList
+        eList = {}
+        for x in range(1,16):
+            if 'eName'+ str(x) in dataCopy:
+                eList[str(x)] = {
+                    'name': dataCopy['eName'+ str(x)],
+                    'qty': dataCopy['eQty'+ str(x)],
+                    'notes': dataCopy['eNotes'+ str(x)]
+                }
+        dataCopy['equipment'] = eList
         data = recipeForm(dataCopy)
         if data.is_valid():
             print('saved')
@@ -40,3 +49,14 @@ def recipeForm_view(request):
         'form': form,
         'modelData': modelData
     })
+    
+def recipeForm_view(request):
+    noFooter = True
+    smallHeader = True
+    sideBar = True
+    
+    return render(request, "recipes/recipeForm.html", {
+        'smallHeader': smallHeader,
+        'noFooter': noFooter,
+        'sideBar': sideBar,
+})
